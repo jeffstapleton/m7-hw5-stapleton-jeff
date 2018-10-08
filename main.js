43fae33a4134803e27d6fe29c9116db0592096d8
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-10-08 00:32:22 -0500

    Append link to linkListItem and linkListItem to ul

--- main.js
+++ main.js
@@ -24,3 +24,5 @@ main.classList.add('text-center');
 
 var link = document.createElement('a');
 var linkListItem = document.createElement('li');
+link.append('linkListItem');
+linkListItem.append('unorderedList');
