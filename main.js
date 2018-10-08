f598aad91085105fa9060a28f1b060506c696710
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-10-07 23:48:15 -0500

    Clean up HTML and main.js,insert main before script

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var main = document.createElement('main');
-
 var pageBody = document.getElementsByTagName('body')[0];
-
 var scriptTag = document.getElementsByTagName('script')[0];
+pageBody.insertBefore(main, scriptTag);
