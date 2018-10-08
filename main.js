a052983ad30ab662160ab2a26d79fd0a4d99bbef
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-10-08 00:23:00 -0500

    Create variables link and linkListItem in js

--- main.js
+++ main.js
@@ -21,3 +21,6 @@ info.appendChild(listItemText);
 header.classList.add('blue');
 
 main.classList.add('text-center');
+
+var link = document.createElement('a');
+var linkListItem = document.createElement('li');
